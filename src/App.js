b55c14bbf0c6441f94f2a155d97aeb9de0d2e858
username <nomadicalli@mail.ru>
2022-03-03 16:52:21 +0300

    tags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import style from "./App.module.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [tagsArr, setTagsArr] = useState([]);
+  const [empty, setEmpty] = useState(true);
+
+  const handleClick = () => {
+    setTagsArr([...tagsArr, message]);
+    setMessage("");
+  };
+
+  const handleChange = (e) => {
+    if (e.target.value !== "") {
+      setEmpty(true);
+    }
+
+    setMessage(e.target.value);
+  };
+
+  const handleDeleteClick = (index) => {
+    setTagsArr(tagsArr.filter((item, id) => index !== id));
+    // setTagsArr([...tagsArr.slice(0, index), ...tagsArr.slice(index + 1)]);
+  };
+
+  const handleBlur = (e) => {
+    setEmpty(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={style.general}>
+      <div className={style.main}>
+        <input
+          className={style.input}
+          value={message}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <button
+          className={style.button}
+          onClick={handleClick}
+          disabled={message ? false : true}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+        <p className={style.error}>
+          {!empty ? "Поле ввода не должно быть пустым!" : ""}
+        </p>
+      </div>
+      <div className={style.tags}>
+        {tagsArr.map((item, index) => (
+          <button className={style.tagsButton}>
+            {item}
+            <span className={style.x} onClick={() => handleDeleteClick(index)}>
+              X
+            </span>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
